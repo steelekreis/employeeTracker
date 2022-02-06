04f39d40ff3c72784fb86ba5c784c67eba4d177f
steelekreis <steele.kreis@gmail.com>
2022-02-06 14:54:30 -0700

    added connection.js file

---
+++ db/connection.js
@@ -0,0 +1,16 @@
+const mysql = require('mysql2');
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  // Your MySQL username,
+  user: 'root',
+  // Your MySQL password
+  password: 'VZd4_5xD!m4f9qM',
+  database: 'employeeDB'
+});
+
+connection.db(function (err) {
+    if (err) throw err;
+});
+
+module.exports = db;
